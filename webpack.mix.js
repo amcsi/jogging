003910810b775ada06864b0c003d5bd8a8f7f4d9
Attila Szeremi <attila@szeremi.org>
2018-02-21 16:56:39 -0500

    Add workaround for hot reloading not working with Laravel Mix 2.0

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,23 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+// Workaround
+// https://github.com/JeffreyWay/laravel-mix/issues/1483#issuecomment-366685986
+// https://github.com/JeffreyWay/laravel-mix/issues/1437
+Mix.listen('configReady', (webpackConfig) => {
+  if (Mix.isUsing('hmr')) {
+    // Remove leading '/' from entry keys
+    webpackConfig.entry = Object.keys(webpackConfig.entry).reduce((entries, entry) => {
+      entries[entry.replace(/^\//, '')] = webpackConfig.entry[entry];
+      return entries;
+    }, {});
+
+    // Remove leading '/' from ExtractTextPlugin instances
+    webpackConfig.plugins.forEach((plugin) => {
+      if (plugin.constructor.name === 'ExtractTextPlugin') {
+        plugin.filename = plugin.filename.replace(/^\//, '');
+      }
+    });
+  }
+});
