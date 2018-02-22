a0a14b0f95b2be3c242e62d24c1a1f351b3c68b5
Attila Szeremi <attila@szeremi.org>
2018-02-22 16:38:14 -0500

    Implement logging out by just removing the token

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -4,6 +4,8 @@
 
         <b-container>
             <login-registration :token="token" />
+
+            <button v-if="token" class="btn btn-warning" @click="logout">Log out</button>
         </b-container>
     </div>
 </template>
@@ -22,14 +24,24 @@
         token: localStorage.getItem('token') || '',
       };
     },
+    methods: {
+      logout() {
+        this.setToken('');
+      }
+    },
+    created() {
+      this.setToken = token => {
+        this.token = token;
+        localStorage.setItem('token', token);
+      };
+    },
     mounted() {
       toastRegisterer(this.$refs.toast);
 
       this.$root.$on('loginSuccess', ({ token }) => {
         toast.displaySuccess('Login successful!');
 
-        this.token = token;
-        localStorage.setItem('token', token);
+        this.setToken(token);
         // Make sure all requests include the token from now on.
         axios.defaults.headers.common.Authorization = `Bearer ${token}`;
       });
