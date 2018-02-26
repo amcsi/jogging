9a6bc554279753c28a6a52614c80f40b47a542a8
Attila Szeremi <attila@szeremi.org>
2018-02-26 18:08:20 -0500

    Replace full raw sql with Laravel query builder

--- app/Http/Controllers/JoggingTime/JoggingTimeByWeekController.php
+++ app/Http/Controllers/JoggingTime/JoggingTimeByWeekController.php
@@ -8,17 +8,28 @@ use App\Http\Controllers\Controller;
 use App\JoggingTime;
 use App\JoggingTime\JoggingTimeAggregateTransformer;
 use App\User;
+use Illuminate\Database\Connection;
 
 /**
  * Controller for jogging times aggregated by week.
  */
 class JoggingTimeByWeekController extends Controller
 {
-    public function index(User $user, JoggingTimeAggregateTransformer $transformer)
+    public function index(User $user, JoggingTimeAggregateTransformer $transformer, Connection $connection)
     {
         $this->authorize('index', [JoggingTime::class, $user]);
 
-        $result = \DB::select(\DB::raw(file_get_contents(__DIR__ . '/aggregateByWeek.sql')));
+        $result = $connection->table('jogging_times')
+            ->select(
+                $connection->raw("strftime('%W', day) WeekNumber"),
+                $connection->raw("max(date(day, 'weekday 0', '-7 day')) first_day"),
+                $connection->raw("max(date(day, 'weekday 0', '-1 day')) last_day"),
+                $connection->raw("COUNT(*) AS GroupedValues"),
+                $connection->raw("SUM(distance_m) AS distance_m"),
+                $connection->raw("SUM(minutes) AS minutes")
+            )
+            ->groupBy('WeekNumber')
+            ->get();
 
         return JsonResponder::respond($result, $transformer);
     }
--- app/Http/Controllers/JoggingTime/aggregateByWeek.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  strftime('%W', day) WeekNumber,
-  max(date(day, 'weekday 0', '-7 day')) first_day,
-  max(date(day, 'weekday 0', '-1 day')) last_day,
-  count(*) AS GroupedValues,
-  SUM(distance_m) AS distance_m,
-  SUM(minutes) AS minutes
-FROM jogging_times
-GROUP BY WeekNumber;
