2bc34109d247a02267db46fffcacfa97dee39db0
Attila Szeremi <attila@szeremi.org>
2018-02-27 15:02:49 -0500

    Ensure a key when looping navbar links

--- resources/assets/js/components/NavBar.vue
+++ resources/assets/js/components/NavBar.vue
@@ -8,7 +8,9 @@
         <b-collapse is-nav id="nav_collapse">
 
             <b-navbar-nav v-if="links.length">
-                <b-nav-item v-for="link in links" :href="'#/' + link.uri">{{ link.label }}</b-nav-item>
+                <b-nav-item v-for="link in links" :key="link.uri + link.label" :href="'#/' + link.uri">{{ link.label
+                    }}
+                </b-nav-item>
             </b-navbar-nav>
 
             <b-navbar-nav class="ml-auto" v-if="userData">
