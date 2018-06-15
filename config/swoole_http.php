0f81ff12a2f6e95dce5b4dc2b20f20213f726e2a
Attila Szeremi <attila@szeremi.org>
2018-06-15 04:24:23 +0200

    Fix Swoole issue with @albertcht's last suggestion...
    https://github.com/swooletw/laravel-swoole/issues/74#issuecomment-397490515

--- config/swoole_http.php
+++ config/swoole_http.php
@@ -73,9 +73,14 @@ return [
     */
     'providers' => [
         Illuminate\Pagination\PaginationServiceProvider::class,
-        Laravel\Passport\PassportServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
+        /*
+         * This must appear after Illuminate\Auth\AuthServiceProvider.
+         *
+         * https://github.com/swooletw/laravel-swoole/issues/74#issuecomment-397490515
+         */
+        Laravel\Passport\PassportServiceProvider::class,
     ],
 
     /*
