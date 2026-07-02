996dc9f9bcbdef9a8c2cac292a8ae1929078a88b
Attila Szeremi <attila@szeremi.org>
2026-07-02 13:52:42 +0200

    Run migrations in start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-php artisan octane:start --server=swoole --host=0.0.0.0 --port=1215
+set -euo pipefail
+
+php artisan migrate --force
+
+exec php artisan octane:start --server=swoole --host=0.0.0.0 --port=1215
